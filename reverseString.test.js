c1b8b971b9ece742ee1f8c174a531b606ce317f7
jcLucio <c.lucio3@gmail.com>
2020-08-13 12:48:45 -0500

    Add empty string test

--- reverseString.test.js
+++ reverseString.test.js
@@ -4,4 +4,7 @@ describe('reverseString', () => {
   test('no argument', () => {
     expect(reverseString()).toBe('');
   });
+  test('empty string', () => {
+    expect(reverseString()).toBe('');
+  });
 });

a20e9cc35e9dc92c6bd264f137442342400148de
jcLucio <c.lucio3@gmail.com>
2020-08-14 17:14:52 -0500

    Add keeping the same case test

--- caesarCipher.test.js
+++ caesarCipher.test.js
@@ -7,4 +7,9 @@ describe('caesar cipher', () => {
   test('wrapping from z to a', function() {
     expect(caesar('z', 1)).toBe('a');
   });
+  test('keeping the same case', function() {
+    expect(caesar('Defend the East Wall of the Castle', 1)).toBe(
+      'Efgfoe uif Fbtu Xbmm pg uif Dbtumf'
+    );
+  });
 });

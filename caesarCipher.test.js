5e6280cd5919f32791910d207fe3b2e5c766b662
jcLucio <c.lucio3@gmail.com>
2020-08-14 17:14:24 -0500

    Add wrapping from z to a test

--- caesarCipher.test.js
+++ caesarCipher.test.js
@@ -4,4 +4,7 @@ describe('caesar cipher', () => {
   test('lowecase word', function() {
     expect(caesar('odin', 1)).toBe('pejo');
   });
+  test('wrapping from z to a', function() {
+    expect(caesar('z', 1)).toBe('a');
+  });
 });

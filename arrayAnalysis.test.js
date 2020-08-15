a22907b354462e7a93aa0cbdbf02cef96d35de91
jcLucio <c.lucio3@gmail.com>
2020-08-15 14:17:35 -0500

    Add negative numbers test

--- arrayAnalysis.test.js
+++ arrayAnalysis.test.js
@@ -15,10 +15,21 @@ describe('analyze', () => {
       expect(analyze([1, 2, 3, 4]).length).toBe(4);
     });
   });
+
   describe('complete object', () => {
     test('is object', () => {
       const actual = typeof analyze([0, 10]);
       const expected = 'object';
+      expect(actual).toEqual(expected);
+    });
+    test('negative numbers', () => {
+      const actual = analyze([5, 2, -34, 12, 7, 125, 67, 0, -495, 13]);
+      const expected = {
+        average: -29.8,
+        min: -495,
+        max: 125,
+        length: 10,
+      };
       expect(actual).toEqual(expected);
     });
   });
